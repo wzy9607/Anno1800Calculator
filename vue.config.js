ca69ac8663a7b15033a493f6abd483e94e95d4ad
Cattī Crūdēlēs <wzy960724@gmail.com>
2019-04-21 14:35:15 +0800

    Misc: Split Webpack chunks.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Anno1800Calculator/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/Anno1800Calculator/" : "/",
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    }
+  }
 };
